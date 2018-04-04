4cdbe321ed53ec011e1809c17bd2231435ffff94
Ashpool <ashpool@xecut.net>
2018-04-04 14:27:06 +0300

    Chain-tree finalization with probabilities calculation. Dump model to JSON file.

--- train.py
+++ train.py
@@ -1,36 +1,51 @@
 import argparse
 import string
-import re
+import json
 from collections import defaultdict
 
 argParser = argparse.ArgumentParser(
     description="Train a Markov model based on text files.")
-#argParser.add_argument()
 
 
-class Tree:
-    @staticmethod
-    def _factory():
-        return defaultdict(Tree._factory)
-
-    def __init__(self):
-        self._tree = Tree._factory()
-
-    def inc_ctx(self, context):
-        subtree = self._tree
-        for node in context[:-1]:
-            subtree = subtree[node]
-        fkey = context[-1]
-        if not subtree.get(fkey):
-            subtree[fkey] = 1
-        else:
-            subtree[fkey] += 1
+# argParser.add_argument()
 
 
 class MarkovModel:
-    def __init__(self, ctxLength):
-        self.chains = Tree()
-        self.ctxLength = ctxLength
+    class Tree:
+        @staticmethod
+        def _factory():
+            return defaultdict(MarkovModel.Tree._factory)
+
+        def __init__(self):
+            self._tree = MarkovModel.Tree._factory()
+
+        def inc_ctx(self, context):
+            subtree = self._tree
+            for node in context[:-1]:
+                subtree = subtree[node]
+            fkey = context[-1]
+            if not subtree.get(fkey):
+                subtree[fkey] = 1
+            else:
+                subtree[fkey] += 1
+
+        def traverse(self, depth, tree=None):
+            if not tree:
+                tree = self._tree
+            if depth > 0:
+                for key, subtree in tree.items():
+                    self.traverse(depth - 1, subtree)
+                return
+            # else we have leaves
+            leafsum = 0
+            for key, leaf in tree.items():
+                leafsum += leaf
+            for key in tree.keys():
+                tree[key] = "%.4f" % (tree[key] / leafsum)
+
+    def __init__(self, ctx_length):
+        self.chains = MarkovModel.Tree()
+        self.ctxLength = ctx_length
 
     def train(self, ifs):
         context = []
@@ -44,8 +59,17 @@ class MarkovModel:
                 self.chains.inc_ctx(context)
                 del context[0]
 
+    def finalize(self):
+        self.chains.traverse(self.ctxLength)
+
+    def dump(self, ofs):
+        json.dump(self.chains._tree, ofs)
+
 
 mm = MarkovModel(2)
 arg_ifs = open("sonnets.txt", 'r')
+arg_ofs = open("model.json", 'w')
 mm.train(arg_ifs)
+mm.finalize()
+mm.dump(arg_ofs)
 pass
