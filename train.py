2c42f9749e729debd3b9c6e3098c483566beddc8
Ashpool <ashpool@xecut.net>
2018-04-07 17:44:59 +0300

    Added __name__ = "__main__" check in scripts & more - Fixes #3 - All file streams are now closed as soon as no longer needed

--- train.py
+++ train.py
@@ -2,43 +2,50 @@ import argparse
 import sys
 from Markov import Model
 
-aparse = argparse.ArgumentParser(
-    description="Train a Markov model based on text files.",
-    add_help=False)
-aparse.add_argument('--ctx', type=int, default=1,
-                    help="Length of context (number of words). 1 by default")
-aparse.add_argument('--input', default=sys.stdin,
-                    type=argparse.FileType('r', encoding='utf8'),
-                    help="Path to the input file. If both this and " \
-                         "--input-dir are ommitted, read from stdin.")
-aparse.add_argument('--input-dir', default=None,
-                    help="Path to the input directory with .txt files.")
-aparse.add_argument('--model', dest='ofs',
-                    type=argparse.FileType('w', encoding='utf8'),
-                    default=sys.stdout,
-                    help="Path to the output model file. "
-                         "Print to stdout by default")
-aparse.add_argument('--lc', action="store_true",
-                    help="Convert input texts to lower case")
-aparse.add_argument('--help', action="help",
-                    help="Display this help message and exit")
+def main():
+    aparse = argparse.ArgumentParser(
+        description="Train a Markov model based on text files.",
+        add_help=False)
+    aparse.add_argument('--ctx', type=int, default=1,
+                        help="Length of context (number of words). 1 by "
+                             "default.")
+    aparse.add_argument('--input', default=sys.stdin,
+                        type=argparse.FileType('r', encoding='utf8'),
+                        help="Path to the input file. If both this and " \
+                             "--input-dir are ommitted, read from stdin.")
+    aparse.add_argument('--input-dir', default=None,
+                        help="Path to the input directory with .txt files.")
+    aparse.add_argument('--model', dest='ofs',
+                        type=argparse.FileType('w', encoding='utf8'),
+                        default=sys.stdout,
+                        help="Path to the output model file. "
+                             "Print to stdout by default")
+    aparse.add_argument('--lc', action="store_true",
+                        help="Convert input texts to lower case")
+    aparse.add_argument('--help', action="help",
+                        help="Display this help message and exit")
 
-args = aparse.parse_args()
+    args = aparse.parse_args()
 
-if args.ctx < 1:
-    print("Please specify a positive --ctx.")
-    exit(1)
+    if args.ctx < 1:
+        print("Please specify a positive --ctx.")
+        exit(1)
 
-markov_model = Model.empty(ctx_length=args.ctx, lower=args.lc)
-if args.input_dir is not None:
-    import os
-    for file in os.listdir(args.input_dir):
-        if file.endswith(".txt"):
-            fpath = os.path.join(args.input_dir, file)
-            with open(fpath, 'r', encoding="utf8") as ifs:
-                markov_model.train(ifs)
-else:
-    markov_model.train(args.input)
+    markov_model = Model.empty(ctx_length=args.ctx, lower=args.lc)
+    if args.input_dir is not None:
+        import os
+        for file in os.listdir(args.input_dir):
+            if file.endswith(".txt"):
+                fpath = os.path.join(args.input_dir, file)
+                with open(fpath, 'r', encoding="utf8") as ifs:
+                    markov_model.train(ifs)
+    else:
+        markov_model.train(args.input)
+        args.ifs.close()
 
-markov_model.dump(args.ofs)
-args.ofs.close()
+    markov_model.dump(args.ofs)
+    args.ofs.close()
+
+
+if __name__ == "__main__":
+    main()
